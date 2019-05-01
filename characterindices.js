941980bc40a1ff62814805b0a5c4cb6d41736d7a
Liam Hetherington <liambhetherington@gmail.com>
2019-05-01 22:28:36 +0000

    streamlined process of indexing each element in an array. negated if statement to push to array

---
+++ characterindices.js
@@ -0,0 +1,15 @@
+function countLetters(string) {
+  var str = {};
+  var noSpace = string.split(' ').join('');
+    for (var i = 0; i < noSpace.length; i++) {
+      var letter = noSpace.charAt(i);
+      if (!str[letter]) {
+        str[letter] = [];
+      }
+
+        str[letter].push(i);
+    }
+    return str;
+};
+
+console.log(countLetters("lighthouse in the house"));
